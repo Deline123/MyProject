522bf7173b14926d941dcf88294d4e2f102b5b17
unknown <Deline123>
2020-12-08 19:15:51 +0200

    groups sprite

--- ProjectX.py
+++ ProjectX.py
@@ -1,8 +1,9 @@
 import pygame as pg 
 from ball import Ball
+from random import randint
 
 pg.init()
-
+pg.time.set_timer(pg.USEREVENT, 2000)
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0, 0)
 BLUE = (0, 0, 255)
@@ -18,12 +19,24 @@ pg.display.set_icon(pg.image.load("assassins.ico"))
 clock = pg.time.Clock()
 FPS = 60
 
-bg = pg.image.load("img/bg.png")
+balls_images = ['ball_bear.png','ball_fox.png','ball_panda.png']
+balls_surf = [pg.image.load('img/images/'+ path).convert_alpha() for path in balls_images]
+
+def createBall(group):
+	indx = randint(0, len(balls_surf)-1)
+	x = randint(20, W-20)
+	speed = randint(1, 4)
+
+	return Ball(x, speed, balls_surf[indx], group)
+
+
+balls = pg.sprite.Group()
+
+bg = pg.image.load("img/images/back1.jpg").convert()
 
 speed = 1
-b1 = Ball(W//2, speed, "img/ball.png")
 
-
+createBall(balls)
 
 flRunning = True
 while flRunning:
@@ -31,13 +44,16 @@ while flRunning:
 		if event.type == pg.QUIT:
 			pg.quit()
 			flRunning = False
+
+		elif event.type == pg.USEREVENT:
+			createBall(balls)
 	sc.blit(bg, (0, 0))
-	sc.blit(b1.image, b1.rect)
+	balls.draw(sc)
 	pg.display.update()
 	
 
 	clock.tick(FPS)
 
-	b1.update(H)
+	balls.update(H)
 
 	
--- ball.py
+++ ball.py
@@ -1,14 +1,15 @@
-import pygame
+import pygame as pg
 
-class Ball(pygame.sprite.Sprite):
-	def __init__(self, x, filename):
-		pygame.sprite.Sprite.__init__(self)
-		self.image = pygame.image.load(filename).convert_alpha()
+class Ball(pg.sprite.Sprite):
+	def __init__(self, x, speed, surf, group):
+		pg.sprite.Sprite.__init__(self)
+		self.image = surf
 		self.rect = self.image.get_rect(center=(x, 0))
 		self.speed = speed
+		self.add(group)
 		
 	def update(self, *args):
 		if self.rect.y < args[0] - 20:
 			self.rect.y += self.speed
 		else:
-			self.rect.y = 0
+			self.kill()
